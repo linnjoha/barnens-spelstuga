9a789a1da6a271e3ec1d426fc9c4ebee84709ffe
linnjoha <linn.wallin@hotmail.com>
2025-02-28 14:21:51 +0100

    moved out the logic to be able to get new word on same level

--- src/pages/Skriva.jsx
+++ src/pages/Skriva.jsx
@@ -9,11 +9,15 @@ const Skriva = () => {
   const [chosenWords, setChosenWords] = useState([...words]);
   const levels = ["2-4 bokstäver", "4-6 bokstäver", "6 + bokstäver"];
 
+  const [randomWord, setRandomWord] = useState(null);
+  const [userAnswer, setUserAnswer] = useState([]);
+  const [shuffledLetters, setShuffledLetters] = useState([]);
+
   const handleLevelDecision = (i) => {
-    console.log(level);
     setLevel(i);
     const chosenLevelWords = words.filter((word) => word.level == i);
     setChosenWords(chosenLevelWords);
+    startGame();
     setIsOpen(false);
   };
 
@@ -21,7 +25,7 @@ const Skriva = () => {
     return (
       <>
         {" "}
-        <label className="p-2" htmlFor="">
+        <label htmlFor="">
           <input
             onClick={() => handleLevelDecision(i)}
             className="mx-2 cursor-pointer bg-white"
@@ -33,33 +37,25 @@ const Skriva = () => {
     );
   };
 
-  const levelsComponents = levels.map((level, i) => (
-    <LevelAlternativs key={i} level={level} i={i} />
-  ));
   const shuffleArray = (array) => {
     let shuffled = [...array];
     for (let i = shuffled.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
     return shuffled;
   };
+
+  const startGame = () => {
+    const word = chosenWords[Math.floor(Math.random() * chosenWords.length)];
+    setRandomWord(word);
+    setUserAnswer(Array(word.word.length).fill("_"));
+    const shuffled = shuffleArray(word.word.split(""));
+    setShuffledLetters(shuffled);
+  };
+
   const GameComponent = () => {
-    const [randomWord, setRandomWord] = useState(null);
-    const [userAnswer, setUserAnswer] = useState([]);
-    const [shuffledLetters, setShuffledLetters] = useState([]);
-
-    useEffect(() => {
-      const word = chosenWords[Math.floor(Math.random() * chosenWords.length)];
-      setRandomWord(word);
-      setUserAnswer(Array(word.word.length).fill("_"));
-      const shuffled = shuffleArray(word.word.split(""));
-      setShuffledLetters(shuffled);
-    }, [chosenWords]);
-
     if (!randomWord) return <p>Laddar...</p>;
-
-    const wordArr = randomWord.word.split("");
 
     const handleDragStart = (e, letter) => {
       e.dataTransfer.setData("letter", letter);
@@ -69,7 +65,6 @@ const Skriva = () => {
       e.preventDefault();
       const letter = e.dataTransfer.getData("letter");
 
-      // Kopiera arrayen och uppdatera rätt position
       const updatedAnswer = [...userAnswer];
       if (updatedAnswer[i] === "_") {
         updatedAnswer[i] = letter;
@@ -78,7 +73,6 @@ const Skriva = () => {
     };
 
     const allowDrop = (e) => e.preventDefault();
-
     const handleRemoveLetter = (i) => {
       const updatedAnswer = [...userAnswer];
       updatedAnswer[i] = "_";
@@ -89,63 +83,86 @@ const Skriva = () => {
       const isCorretct = userAnswer.join("") === randomWord.word;
       if (isCorretct) {
         alert("heja");
-        location.reload();
+        startGame();
       } else alert("prova en gång till");
     };
     return (
       <>
-        <div className="flex gap-2">
-          {shuffledLetters.map((letter, i) => (
-            <div
-              key={i}
-              className="rounded-2xl bg-white w-12 h-16 text-black flex items-center justify-center shadow cursor-pointer"
-              draggable
-              onDragStart={(e) => handleDragStart(e, letter)}
+        <div className="w-full h-screen flex flex-col items-center">
+          <h2 className="text-3xl font-bold p-10">
+            Flytta bokstäverna för att skriva ordet
+          </h2>
+          <div className="flex gap-2 p-4">
+            {shuffledLetters.map((letter, i) => (
+              <div
+                key={i}
+                className="rounded-2xl bg-white w-12 h-16 text-black flex items-center justify-center shadow cursor-pointer"
+                draggable
+                onDragStart={(e) => handleDragStart(e, letter)}
+              >
+                <p className="font-bold">{letter}</p>
+              </div>
+            ))}
+          </div>
+          <div className="flex gap-5">
+            {userAnswer.map((letter, i) => (
+              <div
+                key={i}
+                className={`rounded-2xl w-12 h-16 flex items-center justify-center border-2 ${
+                  letter === "_"
+                    ? "border-gray-400"
+                    : "bg-gray-300 border-black"
+                }`}
+                onDrop={(e) => handleDrop(e, i)}
+                onDragOver={allowDrop}
+                onClick={() => handleRemoveLetter(i)}
+              >
+                <p className="text-3xl font-bold">{letter}</p>
+              </div>
+            ))}
+          </div>
+          <footer className="w-full flex gap-6 self-end justify-end mt-10 pr-4">
+            <button
+              onClick={checkWord}
+              className="bg-amber-400 px-10 py-4  rounded-full font-bold hover:border-2 border-white "
             >
-              <p className="font-bold">{letter}</p>
-            </div>
-          ))}
-        </div>
-
-        <div className="flex gap-5">
-          {userAnswer.map((letter, i) => (
-            <div
-              key={i}
-              className={`rounded-2xl w-12 h-16 flex items-center justify-center border-2 ${
-                letter === "_" ? "border-gray-400" : "bg-green-300 border-black"
-              }`}
-              onDrop={(e) => handleDrop(e, i)}
-              onDragOver={allowDrop}
-              onClick={() => handleRemoveLetter(i)}
+              Rätta ordet
+            </button>
+            <button
+              onClick={startGame}
+              className="bg-amber-400 px-10 py-4 rounded-full font-bold hover:text-white hover:border-2 "
             >
-              <p className="text-3xl font-bold">{letter}</p>
-            </div>
-          ))}
+              Nytt ord
+            </button>
+            <button
+              onClick={() => {
+                setIsOpen(!isOpen), setLevel(null);
+              }}
+              className="bg-white text-black px-10 py-4 rounded-full font-bold hover:text-gray-700 hover:border-1"
+            >
+              Byt svårighetsgrad
+            </button>
+          </footer>
         </div>
-        <button
-          onClick={checkWord}
-          className="bg-amber-400 px-10 py-4 rounded-full font-bold hover:text-white hover:border-2 mt-6"
-        >
-          Rätta ordet
-        </button>
-        <button className="bg-amber-400 px-10 py-4 rounded-full font-bold hover:text-white hover:border-2 mt-6">
-          Nytt ord
-        </button>
       </>
     );
   };
+  const levelsComponents = levels.map((level, i) => (
+    <LevelAlternativs key={i} level={level} i={i} />
+  ));
 
+  const LevelsModal = () => {
+    return (
+      <div className="flex flex-col gap-10 bg-white text-black w-2xl rounded-2xl drop-shadow-lg min-h-72">
+        <h2 className="text-3xl text-center mt-4">Välj svårighetsgrad</h2>
+        {levelsComponents}
+      </div>
+    );
+  };
   return (
     <>
-      <div className="flex flex-col justify-center items-center w-full min-h-full">
-        {isOpen ? (
-          <div className="flex flex-col bg-white text-black w-2xl pb-4">
-            <h2 className="py-4 text-3xl text-center">Välj svårighetsgrad</h2>
-            {levelsComponents}
-          </div>
-        ) : (
-          ""
-        )}
+      <div className="flex flex-col justify-center items-center w-full min-h-screen">
+        {isOpen ? <LevelsModal /> : ""}
         {!isOpen ? <GameComponent /> : ""}
       </div>
     </>
