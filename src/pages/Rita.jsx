325fadfa86cadb62990aadd5761758b5c6e4145f
linnjoha <linn.wallin@hotmail.com>
2025-03-12 14:11:14 +0100

    styling fix

--- src/pages/Rita.jsx
+++ src/pages/Rita.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { colors } from "../constants/colors";
 const Rita = () => {
   const [bgColor, setBgColor] = useState("");
-  const [bigBg, setBigBg] = useState("#ffffff");
+  const [bigBg, setBigBg] = useState({ hex: "#ffffff" });
   const colorArr = [...colors];
   const [x, setX] = useState(null);
   const [y, setY] = useState(null);
@@ -22,15 +22,15 @@ const Rita = () => {
 
   const ColorPalett = () => {
     return (
-      <div className="flex flex-col">
+      <div className="flex flex-col px-20 gap-6">
         {colorArr.map((color) => (
-          <div key={color.number}>
-            <p>{color.number}</p>
+          <div className="flex gap-4 items-center font-bold" key={color.number}>
             <div
               className={`h-10 w-10 rounded-full border-black cursor-pointer`}
               style={{ backgroundColor: `${color.hex}` }}
               onClick={() => setBgColor(color)}
             ></div>
+            <p>{color.number}</p>
           </div>
         ))}
       </div>
@@ -38,25 +38,19 @@ const Rita = () => {
   };
   const GameComponent = () => {
     const checkUserAnswer = () => {
-      if (x + y == bgColor.number) alert("rätt"), startAddition();
+      if (x + y == bigBg.number) alert("rätt"), startAddition();
       else alert("försök igen");
     };
     return (
-      <div className="self-middle flex flex-col gap-10">
+      <div className="flex flex-col items-center gap-6 px-20">
         <div
-          style={{ backgroundColor: `${bigBg}` }}
-          className="h-20 w-20 border-2 border-black cursor-pointer"
-          onClick={() => setBigBg(bgColor.hex)}
-        ></div>
-        <section className="flex  text-black items-center">
-          <div className="rounded-2xl bg-white w-12 h-16  flex items-center justify-center shadow">
-            <p className="font-bold">{x}</p>
-          </div>
-          <p>+</p>
-          <div className="rounded-2xl bg-white w-12 h-16 flex items-center justify-center shadow">
-            <p className="font-bold">{y}</p>
-          </div>
-        </section>
+          style={{ backgroundColor: `${bigBg.hex}` }}
+          className="flex justify-center items-center h-20 w-20 border-2 border-black cursor-pointer text-black font-bold"
+          onClick={() => setBigBg(bgColor)}
+        >
+          {x}+{y}
+        </div>
+        <section className="flex  text-black items-center"></section>
         <button
           className="bg-amber-400 px-10 py-4 rounded-full font-bold hover:text-white hover:border-2 text-white"
           onClick={() => checkUserAnswer()}
@@ -71,12 +65,12 @@ const Rita = () => {
       <h2 className="text-3xl font-bold p-10 text-center">Räkna och rita</h2>
       <div className="flex items-center justify-center">
         <div className="flex flex-row self-start ">
-          <p>vald färg:</p>
+          <ColorPalett />
+          <p className="text-center">vald färg:</p>
           <div
-            className="h-5 w-5 rounded-full"
+            className="h-5 w-5 rounded-full mx-4"
             style={{ backgroundColor: `${bgColor.hex}` }}
           ></div>
-          <ColorPalett />
         </div>
         <GameComponent />
       </div>
